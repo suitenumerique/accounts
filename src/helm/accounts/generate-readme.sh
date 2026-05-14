3298808c146c2e81832eefb7458bd54adc29776b
Romain Sébille <romain.sebille.ext@numerique.gouv.fr>
2026-05-14 11:06:39 +0200

    🧑‍💻(helm) make `generate-readme.sh` works from anywhere
    The current version was only working when launched from its parent
    directory, but naively I tried launching it from the repository root,
    which didn't works.
    
    Signed-off-by: Romain Sébille <romain.sebille.ext@numerique.gouv.fr>

--- src/helm/accounts/generate-readme.sh
+++ src/helm/accounts/generate-readme.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# `realpath` is not POSIX so using `pwd` for maximum compatibility.
+# More info here : https://stackoverflow.com/a/4774063
+BASE_DIR=$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)
+
 if ! docker image ls | grep readme-generator-for-helm; then
   git clone https://github.com/bitnami/readme-generator-for-helm.git /tmp/readme-generator-for-helm
   cd /tmp/readme-generator-for-helm || exit
   docker build -t readme-generator-for-helm:latest .
-  cd $(dirname -- "${BASH_SOURCE[0]}") || exit
 fi
+cd "$BASE_DIR" || exit
 docker run --rm -it -v .:/source -w /source readme-generator-for-helm:latest readme-generator -v values.yaml -r README.md
