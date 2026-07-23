5de3245995d56caf01a3cf9a561182b61dbcfa09
Romain Sébille <romain.sebille.ext@numerique.gouv.fr>
2026-07-23 12:07:53 +0200

    🦺(core) Add PKCE code verifier and challenge validators

--- src/backend/core/tests/test_validators.py
+++ src/backend/core/tests/test_validators.py
@@ -2,12 +2,14 @@
 
 import contextlib
 import random
+import re
 
 from django.core.exceptions import ValidationError
 
 import pytest
 
 from core import validators
+from core.standards import rfc3986
 
 
 @pytest.mark.parametrize(
@@ -33,3 +35,65 @@ def test_sub_validator(value, expected_error):
     """Test validator raise an error on non-ascii character."""
     with expected_error:
         validators.sub_validator(value)
+
+
+@pytest.mark.parametrize(
+    "allowed_characters,value,expected_error",
+    [
+        pytest.param("", "", contextlib.nullcontext(), id="empty"),
+        pytest.param(
+            "",
+            "a",
+            pytest.raises(ValidationError, match="That character is not allowed: a."),
+            id="character_not_allowed",
+        ),
+        pytest.param(
+            "",
+            "abc",
+            pytest.raises(
+                ValidationError, match="Those characters are not allowed: a,b,c."
+            ),
+            id="characters_not_allowed",
+        ),
+        pytest.param(
+            "",
+            "cba",
+            pytest.raises(
+                ValidationError, match="Those characters are not allowed: a,b,c."
+            ),
+            id="characters_not_allowed_are_sorted",
+        ),
+    ],
+)
+def test_characters_validator(allowed_characters, value, expected_error):
+    """Test CharactersValidator()"""
+    with expected_error:
+        validators.CharactersValidator(allowed_characters)(value)
+
+
+@pytest.mark.parametrize(
+    "value,expected_error",
+    [
+        pytest.param("", contextlib.nullcontext(), id="empty"),
+        pytest.param(
+            rfc3986.UNRESERVED_CHARACTERS,
+            contextlib.nullcontext(),
+            id="unreserved",
+        ),
+        pytest.param(
+            rfc3986.RESERVED_CHARACTERS,
+            pytest.raises(
+                ValidationError,
+                match=re.escape(
+                    "Only URI's unreserved characters are allowed. "
+                    "Found: !,#,$,&,',(,),*,+,,,/,:,;,=,?,@,[,]."
+                ),
+            ),
+            id="reserved",
+        ),
+    ],
+)
+def test_uri_unreserved_characters_validator(value, expected_error):
+    """Test URIUnreservedCharactersValidator()"""
+    with expected_error:
+        validators.URIUnreservedCharactersValidator()(value)
