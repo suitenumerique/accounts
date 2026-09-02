e6353a9254e347ebddafde4b6f7dc563557f4dfe
Romain Sébille <romain.sebille.ext@numerique.gouv.fr>
2026-09-02 17:44:28 +0200

    🧑‍💻(core) add state utils
    Signed-off-by: Romain Sébille <romain.sebille.ext@numerique.gouv.fr>

--- src/backend/accounts/settings.py
+++ src/backend/accounts/settings.py
@@ -85,6 +85,7 @@ class Base(AuthenticationSettings, OIDCProviderSettings, Configuration):
     SECRET_KEY = SecretFileValue(None)
     SECRET_KEY_FALLBACKS = SecretFileListValue([])
     SALT_KEY = SecretFileListValue(None)  # django-fernet-encrypted-fields
+    SESSION_STATE_KEY_LENGTH = values.PositiveIntegerValue(32)
 
     # Application definition
     ROOT_URLCONF = "accounts.urls"
